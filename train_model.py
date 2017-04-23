066bbc421b65d4a26104c711b9eee84bd7da07e6
Aniruddha Tapas <aniruddhastapas@gmail.com>
2017-04-23 17:20:06 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,38 +2,30 @@
 
 import numpy as np
 from alexnet import alexnet
-WIDTH = 160
-HEIGHT = 120
+WIDTH = 250
+HEIGHT = 60
 LR = 1e-3
-EPOCHS = 10
-MODEL_NAME = 'pygta5-car-fast-{}-{}-{}-epochs-300K-data.model'.format(LR, 'alexnetv2',EPOCHS)
+EPOCHS = 5
+MODEL_NAME = 'pydinoai-{}-{}-{}-epochs-2k-data.model'.format(LR, 'alexnetv2',EPOCHS)
 
 model = alexnet(WIDTH, HEIGHT, LR)
 
-hm_data = 22
 for i in range(EPOCHS):
-    for i in range(1,hm_data+1):
-        train_data = np.load('training_data-{}-balanced.npy'.format(i))
+    final_train_data = np.load('final_training_data.npy')
 
-        train = train_data[:-100]
-        test = train_data[-100:]
+    train = final_train_data[:-2500]
+    test = final_train_data[-2500:]
 
-        X = np.array([i[0] for i in train]).reshape(-1,WIDTH,HEIGHT,1)
-        Y = [i[1] for i in train]
+    X = np.array([i[0] for i in train]).reshape(-1,WIDTH,HEIGHT,1)
+    Y = [i[1] for i in train]
 
-        test_x = np.array([i[0] for i in test]).reshape(-1,WIDTH,HEIGHT,1)
-        test_y = [i[1] for i in test]
+    test_x = np.array([i[0] for i in test]).reshape(-1,WIDTH,HEIGHT,1)
+    test_y = [i[1] for i in test]
 
-        model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}), 
-            snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+    model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}), 
+        snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
 
-        model.save(MODEL_NAME)
-
+    model.save(MODEL_NAME)
 
 
 # tensorboard --logdir=foo:C:/path/to/log
-
-
-
-
-
