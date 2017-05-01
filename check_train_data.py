2d0a24cd68297c13fe8a4d2cc839c1b7e40a8e7f
Aniruddha Tapas <aniruddhastapas@gmail.com>
2017-05-01 19:52:33 +0530

    Added PyDinoAI scripts

---
+++ check_train_data.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+from collections import Counter
+from random import shuffle
+
+train = np.load("final_training_data.npy")
+print (len(train))
+
+for i,data in enumerate(train):
+    img = data[0]
+    choice = data[1]
+
+    #if choice == [0,0,1]:
+    cv2.imshow('check_training_data',img)
+    print(str(i) + " " +str(choice))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
+    
